48c309887598420c1a369c2b945cf7ffa299ee46
KimTaeO <taeo0328@gmail.com>
2023-03-28 20:57:52 +0900

    :tada: initial setting

---
+++ src/main/kotlin/com/grapefruit/aid/admin/AdminApplication.kt
@@ -0,0 +1,11 @@
+package com.grapefruit.aid.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminApplication>(*args)
+}
---
+++ src/test/kotlin/com/grapefruit/aid/admin/AdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grapefruit.aid.admin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
