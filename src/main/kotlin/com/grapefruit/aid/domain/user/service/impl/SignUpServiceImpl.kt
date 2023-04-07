68d4647426dfa010292de58db3b6c0cf53b7c043
KimTaeO <taeo0328@gmail.com>
2023-04-07 23:05:36 +0900

    :sparkles: 서비스 어노테이션 등록

--- src/main/kotlin/com/grapefruit/aid/domain/user/service/impl/SignUpServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/user/service/impl/SignUpServiceImpl.kt
@@ -5,7 +5,11 @@ import com.grapefruit.aid.domain.user.repository.UserRepository
 import com.grapefruit.aid.domain.user.exception.UserDuplicateException
 import com.grapefruit.aid.domain.user.presentation.dto.request.SignUpReqDto
 import com.grapefruit.aid.domain.user.service.SignUpService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional(rollbackFor = [Exception::class])
 class SignUpServiceImpl(
     private val userRepository: UserRepository
 ): SignUpService {
