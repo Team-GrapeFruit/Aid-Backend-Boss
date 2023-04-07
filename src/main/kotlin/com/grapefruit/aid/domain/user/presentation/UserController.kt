a033751a6963992a8f97ad17e7f73b719a314229
KimTaeO <taeo0328@gmail.com>
2023-04-07 23:05:59 +0900

    :sparkles: 회원가입 controller 작성

---
+++ src/main/kotlin/com/grapefruit/aid/domain/user/presentation/UserController.kt
@@ -0,0 +1,23 @@
+package com.grapefruit.aid.domain.user.presentation
+
+import com.grapefruit.aid.domain.user.presentation.dto.request.SignUpReqDto
+import com.grapefruit.aid.domain.user.service.SignUpService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val signUpService: SignUpService
+) {
+    @PostMapping("/signup")
+    fun signUp(@RequestBody @Valid signUpReqDto: SignUpReqDto): ResponseEntity<Void> {
+        signUpService.execute(signUpReqDto)
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+}
