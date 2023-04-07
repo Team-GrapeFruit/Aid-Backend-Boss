964987ad1a91e03da0430ce2d1cd9175582f60b1
KimTaeO <taeo0328@gmail.com>
2023-04-07 23:07:20 +0900

    :sparkles: url변경

--- src/main/kotlin/com/grapefruit/aid/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/user/presentation/UserController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/auth")
 class UserController(
     private val signUpService: SignUpService
 ) {
