1d98c4dde8d0d7eb98852299eac819440b82e023
KimTaeO <taeo0328@gmail.com>
2023-04-09 00:34:09 +0900

    :sparkles: 로그인 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/user/presentation/UserController.kt
@@ -1,6 +1,9 @@
 package com.grapefruit.aid.domain.user.presentation
 
+import com.grapefruit.aid.domain.user.presentation.dto.request.SignInReqDto
 import com.grapefruit.aid.domain.user.presentation.dto.request.SignUpReqDto
+import com.grapefruit.aid.domain.user.presentation.dto.response.SignInResDto
+import com.grapefruit.aid.domain.user.service.SignInService
 import com.grapefruit.aid.domain.user.service.SignUpService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,11 +16,18 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/auth")
 class UserController(
-    private val signUpService: SignUpService
+    private val signUpService: SignUpService,
+    private val signInService: SignInService
 ) {
     @PostMapping("/signup")
     fun signUp(@RequestBody @Valid signUpReqDto: SignUpReqDto): ResponseEntity<Void> {
         signUpService.execute(signUpReqDto)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PostMapping
+    fun signIn(@RequestBody @Valid signInReqDto: SignInReqDto): ResponseEntity<SignInResDto> {
+        val result = signInService.execute(signInReqDto)
+        return ResponseEntity.ok(result)
     }
 }
