67d44b882db9e6e80d1c9c14bfaa30f12e5557a6
KimTaeO <taeo0328@gmail.com>
2023-04-09 16:26:46 +0900

    :sparkles: security config 세팅

--- src/main/kotlin/com/grapefruit/aid/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/grapefruit/aid/global/security/SecurityConfig.kt
@@ -2,13 +2,45 @@ package com.grapefruit.aid.global.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsUtils
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .requestMatchers(RequestMatcher { request ->
+                CorsUtils.isPreFlightRequest(request)
+            }).permitAll()
+
+            .antMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+            .antMatchers(HttpMethod.POST, "/auth").permitAll()
+            .anyRequest().denyAll()
+
+            .and().build()
+
+
+
+    }
     @Bean
     fun passwordEncoder(): PasswordEncoder =
         BCryptPasswordEncoder()
