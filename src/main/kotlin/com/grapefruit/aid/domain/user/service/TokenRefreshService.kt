dbba0d80231abe4f0a109266c455b43d1b4b1008
KimTaeO <taeo0328@gmail.com>
2023-04-10 11:22:12 +0900

    :sparkles: 토큰 refresh 리턴값 설정

--- src/main/kotlin/com/grapefruit/aid/domain/user/service/TokenRefreshService.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/user/service/TokenRefreshService.kt
@@ -1,5 +1,7 @@
 package com.grapefruit.aid.domain.user.service
 
+import com.grapefruit.aid.domain.user.presentation.dto.response.TokenRefreshResDto
+
 interface TokenRefreshService {
-    fun execute(refreshToken: String)
+    fun execute(refreshToken: String): TokenRefreshResDto
 }
--- src/main/kotlin/com/grapefruit/aid/domain/user/service/impl/TokenRefreshServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/user/service/impl/TokenRefreshServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.grapefruit.aid.domain.user.service.impl
 
 import com.grapefruit.aid.domain.user.entity.RefreshToken
+import com.grapefruit.aid.domain.user.presentation.dto.response.TokenRefreshResDto
 import com.grapefruit.aid.domain.user.repository.RefreshTokenRepository
 import com.grapefruit.aid.domain.user.service.TokenRefreshService
 import com.grapefruit.aid.global.security.exception.ExpiredTokenException
@@ -11,7 +12,7 @@ class TokenRefreshServiceImpl(
     private val refreshTokenRepository: RefreshTokenRepository,
     private val tokenProvider: TokenProvider
 ): TokenRefreshService {
-    override fun execute(requestRefreshToken: String) {
+    override fun execute(requestRefreshToken: String):TokenRefreshResDto {
         val requestRefreshToken: String = tokenProvider.parseToken(requestRefreshToken) ?: throw InvalidTokenException()
         val id = tokenProvider.exactEmailFromRefreshToken(requestRefreshToken)
         val existingRefreshToken = refreshTokenRepository.findByToken(requestRefreshToken) ?: throw ExpiredTokenException()
@@ -24,6 +25,6 @@ class TokenRefreshServiceImpl(
         val refreshToken = RefreshToken(id, refresh)
         refreshTokenRepository.save(refreshToken)
 
-
+        return TokenRefreshResDto(access, refresh, tokenProvider.accessExpiredTime)
     }
 }
