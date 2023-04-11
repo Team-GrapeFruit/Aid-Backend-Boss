e8895033476b4026fcb995528351dc5e550fb42e
KimTaeO <taeo0328@gmail.com>
2023-04-11 13:43:01 +0900

    :sparkles: 가게 등록 서비스 작성

--- src/main/kotlin/com/grapefruit/aid/domain/store/service/impl/CreateStoreServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/store/service/impl/CreateStoreServiceImpl.kt
@@ -1,16 +1,22 @@
 package com.grapefruit.aid.domain.store.service.impl
 
+import com.grapefruit.aid.domain.store.entity.Store
 import com.grapefruit.aid.domain.store.presentation.dto.request.CreateStoreReqDto
+import com.grapefruit.aid.domain.store.repository.StoreRepository
 import com.grapefruit.aid.domain.store.service.CreateStoreService
-import com.grapefruit.aid.domain.user.repository.UserRepository
 import com.grapefruit.aid.domain.user.util.UserUtil
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional(rollbackFor = [Exception::class])
 class CreateStoreServiceImpl(
     private val userUtil: UserUtil,
-    private val userRepository: UserRepository
+    private val storeRepository: StoreRepository
 ): CreateStoreService {
     override fun execute(createStoreReqDto: CreateStoreReqDto) {
         val user = userUtil.currentUser()
-
+        val store = Store(createStoreReqDto, user)
+        storeRepository.save(store)
     }
 }
