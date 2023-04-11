c3db9abf87962535b064c6890af4dbb582505204
KimTaeO <taeo0328@gmail.com>
2023-04-11 23:28:54 +0900

    :sparkles: 가게 삭제 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/store/presentation/StoreController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/store/presentation/StoreController.kt
@@ -4,12 +4,15 @@ import com.grapefruit.aid.domain.store.presentation.dto.request.CreateStoreReqDt
 import com.grapefruit.aid.domain.store.presentation.dto.request.ModifyStoreReqDto
 import com.grapefruit.aid.domain.store.presentation.dto.response.GetAllMyStoreResDto
 import com.grapefruit.aid.domain.store.service.CreateStoreService
+import com.grapefruit.aid.domain.store.service.DeleteStoreService
 import com.grapefruit.aid.domain.store.service.GetAllMyStoreService
 import com.grapefruit.aid.domain.store.service.ModifyStoreService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,7 +24,8 @@ import javax.validation.Valid
 class StoreController(
     private val createStoreService: CreateStoreService,
     private val getAllMyStoreService: GetAllMyStoreService,
-    private val modifyStoreService: ModifyStoreService
+    private val modifyStoreService: ModifyStoreService,
+    private val deleteStoreService: DeleteStoreService
 ) {
     @PostMapping
     fun createStore(@RequestBody @Valid createStoreReqDto: CreateStoreReqDto): ResponseEntity<Void> {
@@ -38,6 +42,12 @@ class StoreController(
     @PatchMapping
     fun modifyStore(modifyStoreReqDto: ModifyStoreReqDto): ResponseEntity<Void> {
         modifyStoreService.execute(modifyStoreReqDto)
+        return ResponseEntity.noContent().build()
+    }
+
+    @DeleteMapping("/{store_id}")
+    fun deleteStore(@PathVariable("store_id") storeId: Long): ResponseEntity<Void> {
+        deleteStoreService.execute(storeId)
         return ResponseEntity.noContent().build()
     }
 }
