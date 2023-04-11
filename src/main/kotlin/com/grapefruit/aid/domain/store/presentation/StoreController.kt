b94cc972503d22ff097c876d6fefa8cdb5256111
KimTaeO <taeo0328@gmail.com>
2023-04-11 15:50:20 +0900

    :sparkles: 자신이 소유한 가게 리스트 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/store/presentation/StoreController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/store/presentation/StoreController.kt
@@ -1,9 +1,12 @@
 package com.grapefruit.aid.domain.store.presentation
 
 import com.grapefruit.aid.domain.store.presentation.dto.request.CreateStoreReqDto
+import com.grapefruit.aid.domain.store.presentation.dto.response.GetAllMyStoreResDto
 import com.grapefruit.aid.domain.store.service.CreateStoreService
+import com.grapefruit.aid.domain.store.service.GetAllMyStoreService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,11 +16,18 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/store")
 class StoreController(
-    private val createStoreService: CreateStoreService
+    private val createStoreService: CreateStoreService,
+    private val getAllMyStoreService: GetAllMyStoreService
 ) {
     @PostMapping
     fun createStore(@RequestBody @Valid createStoreReqDto: CreateStoreReqDto): ResponseEntity<Void> {
         createStoreService.execute(createStoreReqDto)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun getAllMyStore(): ResponseEntity<GetAllMyStoreResDto> {
+        val result = getAllMyStoreService.execute()
+        return ResponseEntity.ok(result)
     }
 }
