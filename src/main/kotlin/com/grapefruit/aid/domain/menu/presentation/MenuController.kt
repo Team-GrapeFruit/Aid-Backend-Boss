fcce23ac141fbf22e6f0ace54e2f0348820dbcb8
KimTaeO <taeo0328@gmail.com>
2023-04-13 00:52:53 +0900

    :sparkles: 메뉴 삭제 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
@@ -3,9 +3,11 @@ package com.grapefruit.aid.domain.menu.presentation
 import com.grapefruit.aid.domain.menu.presentation.dto.request.CreateMenuReqDto
 import com.grapefruit.aid.domain.menu.presentation.dto.request.ModifyMenuReqDto
 import com.grapefruit.aid.domain.menu.service.CreateMenuService
+import com.grapefruit.aid.domain.menu.service.DeleteMenuService
 import com.grapefruit.aid.domain.menu.service.ModifyMenuService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,7 +20,8 @@ import javax.validation.Valid
 @RequestMapping("/menu")
 class MenuController(
     private val createMenuService: CreateMenuService,
-    private val modifyMenuService: ModifyMenuService
+    private val modifyMenuService: ModifyMenuService,
+    private val deleteMenuService: DeleteMenuService
 ) {
     @PostMapping("/{store_id}")
     fun createMenu(@PathVariable("store_id") storeId: Long, @RequestBody @Valid createMenuReqDto: CreateMenuReqDto): ResponseEntity<Void> {
@@ -32,4 +35,9 @@ class MenuController(
         return ResponseEntity.noContent().build()
     }
 
+    @DeleteMapping("/{menu_id}")
+    fun deleteMenu(@PathVariable("menu_id") menuId: Long): ResponseEntity<Void> {
+        deleteMenuService.execute(menuId)
+        return ResponseEntity.noContent().build()
+    }
 }
