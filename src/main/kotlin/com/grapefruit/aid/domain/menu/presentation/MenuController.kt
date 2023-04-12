8a64444c5b26bf1409221bea7c7bcdd8e651739c
KimTaeO <taeo0328@gmail.com>
2023-04-13 00:45:10 +0900

    :sparkles: 메뉴 수정 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
@@ -1,9 +1,12 @@
 package com.grapefruit.aid.domain.menu.presentation
 
 import com.grapefruit.aid.domain.menu.presentation.dto.request.CreateMenuReqDto
+import com.grapefruit.aid.domain.menu.presentation.dto.request.ModifyMenuReqDto
 import com.grapefruit.aid.domain.menu.service.CreateMenuService
+import com.grapefruit.aid.domain.menu.service.ModifyMenuService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,11 +17,19 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/menu")
 class MenuController(
-    private val createMenuService: CreateMenuService
+    private val createMenuService: CreateMenuService,
+    private val modifyMenuService: ModifyMenuService
 ) {
     @PostMapping("/{store_id}")
     fun createMenu(@PathVariable("store_id") storeId: Long, @RequestBody @Valid createMenuReqDto: CreateMenuReqDto): ResponseEntity<Void> {
         createMenuService.execute(storeId, createMenuReqDto)
         return ResponseEntity(HttpStatus.CREATED)
     }
+
+    @PatchMapping("/{menu_id}")
+    fun modifyMenu(@PathVariable("menu_id") menuId: Long, @RequestBody @Valid modifyMenuReqDto: ModifyMenuReqDto): ResponseEntity<Void> {
+        modifyMenuService.execute(menuId, modifyMenuReqDto)
+        return ResponseEntity.noContent().build()
+    }
+
 }
