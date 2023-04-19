1b13cc68d3f92eb2973ea59d0b69f292504c6372
KimTaeO <taeo0328@gmail.com>
2023-04-19 18:03:14 +0900

    :sparkles: 카테고리 삭제 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/category/presentation/CategoryController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/category/presentation/CategoryController.kt
@@ -2,8 +2,10 @@ package com.grapefruit.aid.domain.category.presentation
 
 import com.grapefruit.aid.domain.category.presentation.dto.request.CreateCategoryReqDto
 import com.grapefruit.aid.domain.category.service.CreateCategoryService
+import com.grapefruit.aid.domain.category.service.DeleteCategoryService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,11 +16,18 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/category")
 class CategoryController(
-    private val createCategoryService: CreateCategoryService
+    private val createCategoryService: CreateCategoryService,
+    private val deleteCategoryService: DeleteCategoryService
 ) {
     @PostMapping("/{store_id}")
     fun createCategory(@PathVariable("store_id") storeId: Long, @RequestBody @Valid createCategoryReqDto: CreateCategoryReqDto):ResponseEntity<Void> {
         createCategoryService.execute(storeId, createCategoryReqDto)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @DeleteMapping("/{category_id}")
+    fun deleteCategory(@PathVariable("category_id") categoryId: Long): ResponseEntity<Void> {
+        deleteCategoryService.execute(categoryId)
+        return ResponseEntity.ok().build()
     }
 }
