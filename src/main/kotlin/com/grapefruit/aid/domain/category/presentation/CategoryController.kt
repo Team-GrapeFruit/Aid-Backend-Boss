62c248d4c87787d79817c8a7facc48ae047ef119
KimTaeO <taeo0328@gmail.com>
2023-04-19 22:37:12 +0900

    :sparkles: 메뉴에 카테고리 등록 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/category/presentation/CategoryController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/category/presentation/CategoryController.kt
@@ -3,13 +3,16 @@ package com.grapefruit.aid.domain.category.presentation
 import com.grapefruit.aid.domain.category.presentation.dto.request.CreateCategoryReqDto
 import com.grapefruit.aid.domain.category.service.CreateCategoryService
 import com.grapefruit.aid.domain.category.service.DeleteCategoryService
+import com.grapefruit.aid.domain.category.service.EnrollCategoryService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -17,7 +20,8 @@ import javax.validation.Valid
 @RequestMapping("/category")
 class CategoryController(
     private val createCategoryService: CreateCategoryService,
-    private val deleteCategoryService: DeleteCategoryService
+    private val deleteCategoryService: DeleteCategoryService,
+    private val enrollCategoryService: EnrollCategoryService
 ) {
     @PostMapping("/{store_id}")
     fun createCategory(@PathVariable("store_id") storeId: Long, @RequestBody @Valid createCategoryReqDto: CreateCategoryReqDto):ResponseEntity<Void> {
@@ -29,5 +33,11 @@ class CategoryController(
     fun deleteCategory(@PathVariable("category_id") categoryId: Long): ResponseEntity<Void> {
         deleteCategoryService.execute(categoryId)
         return ResponseEntity.ok().build()
+    }
+
+    @PostMapping
+    fun enrollCategory(@RequestParam menuId: Long, @RequestParam categoryId: Long): ResponseEntity<Void> {
+        enrollCategoryService.execute(menuId,categoryId)
+        return ResponseEntity(HttpStatus.CREATED)
     }
 }
