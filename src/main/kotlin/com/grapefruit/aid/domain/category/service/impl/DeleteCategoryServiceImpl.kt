252fc158b3732696fe700a68d0e9fda7e8d935c6
KimTaeO <taeo0328@gmail.com>
2023-04-20 22:31:25 +0900

    :sparkles: 카테고리 user가 가게 소유주인지 확인하는 로직 추가

--- src/main/kotlin/com/grapefruit/aid/domain/category/service/impl/DeleteCategoryServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/category/service/impl/DeleteCategoryServiceImpl.kt
@@ -3,6 +3,8 @@ package com.grapefruit.aid.domain.category.service.impl
 import com.grapefruit.aid.domain.category.exception.CategoryNotFoundException
 import com.grapefruit.aid.domain.category.repository.CategoryRepository
 import com.grapefruit.aid.domain.category.service.DeleteCategoryService
+import com.grapefruit.aid.domain.store.exception.UserMismatchException
+import com.grapefruit.aid.domain.user.util.UserUtil
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,10 +12,13 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(rollbackFor = [Exception::class])
 class DeleteCategoryServiceImpl(
-    private val categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository,
+    private val userUtil: UserUtil
 ): DeleteCategoryService {
     override fun execute(categoryId: Long) {
         val category = categoryRepository.findByIdOrNull(categoryId) ?: throw CategoryNotFoundException()
+        if(category.store.user != userUtil.currentUser())
+            throw UserMismatchException()
         categoryRepository.delete(category)
     }
 
--- src/main/kotlin/com/grapefruit/aid/domain/category/service/impl/EnrollCategoryServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/category/service/impl/EnrollCategoryServiceImpl.kt
@@ -7,6 +7,8 @@ import com.grapefruit.aid.domain.category.repository.MenuCategoryRepository
 import com.grapefruit.aid.domain.category.service.EnrollCategoryService
 import com.grapefruit.aid.domain.menu.exception.MenuNotFoundException
 import com.grapefruit.aid.domain.menu.repository.MenuRepository
+import com.grapefruit.aid.domain.store.exception.UserMismatchException
+import com.grapefruit.aid.domain.user.util.UserUtil
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,10 +18,15 @@ import org.springframework.transaction.annotation.Transactional
 class EnrollCategoryServiceImpl(
     private val menuCategoryRepository: MenuCategoryRepository,
     private val menuRepository: MenuRepository,
-    private val categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository,
+    private val userUtil: UserUtil
 ): EnrollCategoryService {
     override fun execute(menuId: Long, categoryId: Long) {
         val menu = menuRepository.findByIdOrNull(menuId) ?: throw MenuNotFoundException()
+
+        if(menu.store.user != userUtil.currentUser())
+            throw UserMismatchException()
+        
         val category = categoryRepository.findByIdOrNull(categoryId) ?: throw CategoryNotFoundException()
         menuCategoryRepository.save(MenuAndCategory(menu, category))
     }
