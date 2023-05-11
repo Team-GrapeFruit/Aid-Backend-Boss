88cbd498f97a73181f492130d399c92ea10aba6c
KimTaeO <taeo0328@gmail.com>
2023-05-11 15:36:57 +0900

    :white_check_mark: 토큰 리프레시 컨트롤러 테스트코드 작성

---
+++ src/test/kotlin/com/grapefruit/aid/domain/user/presentation/TokenRefreshControllerTest.kt
@@ -0,0 +1,50 @@
+package com.grapefruit.aid.domain.user.presentation
+
+import com.grapefruit.aid.domain.user.presentation.dto.response.TokenRefreshResDto
+import com.grapefruit.aid.domain.user.service.SignInService
+import com.grapefruit.aid.domain.user.service.SignUpService
+import com.grapefruit.aid.domain.user.service.TokenRefreshService
+import com.grapefruit.aid.domain.user.service.UserDeleteService
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.ZonedDateTime
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest
+@AutoConfigureMockMvc
+class TokenRefreshControllerTest: BehaviorSpec({
+    val signUpService = mockk<SignUpService>()
+    val signInService = mockk<SignInService>()
+    val tokenRefreshService = mockk<TokenRefreshService>()
+    val userDeleteService = mockk<UserDeleteService>()
+    val userController = UserController(signUpService, signInService, tokenRefreshService, userDeleteService)
+
+    Given("토큰 리프레시 시도 시") {
+        `when`("요청이 왔을 때") {
+            every { tokenRefreshService.execute("refreshToken") } returns TokenRefreshResDto(
+                accessToken = "newAccessToken",
+                refreshToken = "newRefreshToken",
+                expiresAt = ZonedDateTime.now()
+            )
+            val result = userController.tokenRefresh("refreshToken")
+            then("서비스가 한번은 실행되어야 함") {
+                verify(exactly = 1) { tokenRefreshService.execute("refreshToken") }
+            }
+            then("response status should be ok") {
+                result.statusCode shouldBe HttpStatus.OK
+            }
+        }
+    }
+})
