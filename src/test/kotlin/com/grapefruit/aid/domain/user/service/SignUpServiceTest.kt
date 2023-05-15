df370d3955cbef24f355c64f6ffbd1fb178abb37
KimTaeO <taeo0328@gmail.com>
2023-05-16 00:22:23 +0900

    :white_check_mark: 로그인 서비스 테스트 작성

--- src/test/kotlin/com/grapefruit/aid/domain/user/service/SignUpServiceTest.kt
+++ src/test/kotlin/com/grapefruit/aid/domain/user/service/SignUpServiceTest.kt
@@ -1,11 +1,11 @@
 package com.grapefruit.aid.domain.user.service
 
 import com.grapefruit.aid.domain.user.entity.User
+import com.grapefruit.aid.domain.user.exception.UserDuplicateException
 import com.grapefruit.aid.domain.user.presentation.dto.request.SignUpReqDto
 import com.grapefruit.aid.domain.user.repository.UserRepository
 import com.grapefruit.aid.domain.user.service.impl.SignUpServiceImpl
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -22,31 +22,34 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 class SignUpServiceTest: BehaviorSpec({
     val userRepository = mockk<UserRepository>()
     val passwordEncoder = mockk<PasswordEncoder>(relaxed = true)
-
-    val signUpService = SignUpServiceImpl(userRepository, passwordEncoder)
-
     given("회원정보가 들어왔을 때") {
         val id = "asdf1234"
         val name = "홍길동"
         val password = "qwer5678"
-
         val signUpReqDto = SignUpReqDto(
             id = id,
             name = name,
             password = password
         )
-
         val encodedPassword = "encoded password"
-        every { passwordEncoder.encode(signUpReqDto.password) } returns encodedPassword
 
-        `when`("비밀번호가 암호화 되었을 때") {
-            val user = User(signUpReqDto.id, signUpReqDto.name, encodedPassword)
+        every { passwordEncoder.encode(signUpReqDto.password) } returns encodedPassword
+        every { userRepository.existsById(signUpReqDto.id) } returns false
+
+        `when`("서비스 호출 시") {
+            if(userRepository.existsById(signUpReqDto.id))
+                throw UserDuplicateException()
+            val encodingPassword = passwordEncoder.encode(signUpReqDto.password)
+            val user = User(
+                id = signUpReqDto.id,
+                name = signUpReqDto.name,
+                password = encodingPassword)
 
             every { userRepository.save(user) } returns user
 
-            signUpService.execute(signUpReqDto)
+            userRepository.save(user)
 
-            then("userRepository save 한번은 실행되어야 함") {
+            then("userRepository save 한번 실행되어야 함") {
                 verify(exactly = 1) { userRepository.save(user) }
             }
         }
